e651f05fbe3e5db230551325f8ce2d949ec64805
mroh004 <mroh004@dev>
2012-08-09 11:25:08 +1200

    implemeting main form an login form

---
+++ ajFileSync/src/main/java/aj/apps/filesync/AjFileSync.java
@@ -0,0 +1,15 @@
+package aj.apps.filesync;
+
+import aj.apps.filesync.internal.MainFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class AjFileSync 
+{
+    public static void main( String[] args )
+    {
+        MainFrame.main(args);
+    }
+}
